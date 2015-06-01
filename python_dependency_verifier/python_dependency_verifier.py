3ea05ba78f7c48f8f23fff582c6d2adbb5f50102
gilzellner <gil.zellner@gmail.com>
2015-06-01 11:52:53 +0300

    fixed naming, caps for docstrings

--- python_dependency_verifier/python_dependency_verifier.py
+++ python_dependency_verifier/python_dependency_verifier.py
@@ -23,8 +23,8 @@ import json
 filename_to_search_for = "setup.py"
 
 
-def string_dependency_to_dict(string_dependency):
-    """receives an item from install_requires field
+def _string_dependency_to_dict(string_dependency):
+    """Receives an item from install_requires field
     in the form of ITEMNAME RELATION VERSION
     like pika==0.9.13
     and returns the following dictionary:
@@ -45,7 +45,7 @@ def string_dependency_to_dict(string_dependency):
 
 
 def get_latest_version_number(package_name):
-    """receives a package name and
+    """Receives a package name and
     returns the latest version its available in from pip"""
     pkg, all_versions = CheeseShop().query_versions_pypi(package_name)
     if len(all_versions):
@@ -53,23 +53,23 @@ def get_latest_version_number(package_name):
     return None
 
 
-def fix_unequal_conditions(string):
-    """makes <> conditions human readable"""
+def _fix_unequal_conditions(string):
+    """Makes <> conditions human readable"""
     return string.replace(",<", "<")
 
 
-def remove_whitespace_from_equals(string):
-    """receives string and returns it without inline comments"""
+def _remove_whitespace_from_equals(string):
+    """Receives string and returns it without inline comments"""
     return string.replace(" =", "=").replace("= ", "=")
 
 
-def remove_comments(string):
-    """receives string and returns it without inline comments"""
+def _remove_comments(string):
+    """Receives string and returns it without inline comments"""
     return re.sub(re.compile("#.*?\n"), "", string)
 
 
-def get_file_contents(path_to_file):
-    """receives a path to file and returns its contents as string"""
+def _get_file_contents(path_to_file):
+    """Receives a path to file and returns its contents as string"""
     contents = None
     try:
         with open(path_to_file, 'r') as f:
@@ -81,15 +81,15 @@ def get_file_contents(path_to_file):
     return contents
 
 
-def filter_list_for_regex(list_of_strings, filter_regex):
+def _filter_list_for_regex(list_of_strings, filter_regex):
     return [string for string in list_of_strings
             if not re.match(re.compile(filter_regex), string)]
 
-def remove_quotes_and_whitespace_from_list(list):
-    return [remove_quotes_and_whitespace(x) for x in list]
+def _remove_quotes_and_whitespace_from_list(list):
+    return [_remove_quotes_and_whitespace(x) for x in list]
 
 
-def remove_quotes_and_whitespace(string):
+def _remove_quotes_and_whitespace(string):
     return string.replace(" ", "").replace(
         "\'", "").replace("\"", "")
 
@@ -110,7 +110,7 @@ class PythonSetuptoolsDependencyCheckerForFile():
         return [x for x in self._list_of_dependencies if not x["is_locked"]]
 
     def _get_append_list(self, file_contents):
-        """receives a string: file_contents which should be a setup.py file,
+        """Receives a string: file_contents which should be a setup.py file,
         and a string regex: ignore_this
         returns a list of requirements which
         are added via the append clause
@@ -124,13 +124,13 @@ class PythonSetuptoolsDependencyCheckerForFile():
         return self._list_of_unprocessed_dependencies
 
     def _get_install_requires_field_contents(self, file_contents):
-        """receives a string: file_contents which should be a setup.py file,
+        """Receives a string: file_contents which should be a setup.py file,
         returns a list of requirements which
         are added via the install_requires variable"""
         phrase_start = "install_requires=["
-        file_contents = fix_unequal_conditions(
-            remove_comments(remove_whitespace_from_equals(
-                remove_quotes_and_whitespace(file_contents))))
+        file_contents = _fix_unequal_conditions(
+            _remove_comments(_remove_whitespace_from_equals(
+                _remove_quotes_and_whitespace(file_contents))))
         if phrase_start in file_contents:
             file_contents = ''.join(file_contents.splitlines())
             starts_at = file_contents.find(phrase_start)
@@ -141,7 +141,7 @@ class PythonSetuptoolsDependencyCheckerForFile():
         return self._list_of_unprocessed_dependencies
 
     def _get_dependency_with_latest_version(self, list_of_dependencies):
-        """receives a list of items from install_requires field
+        """Receives a list of items from install_requires field
         in the form of ITEMNAME RELATION VERSION
         like pika==0.9.13
         and returns the following dictionary:
@@ -152,16 +152,16 @@ class PythonSetuptoolsDependencyCheckerForFile():
                 for dependency in list_of_dependencies if dependency]
 
     def _process_dependency_list(self):
-        self._list_of_unprocessed_dependencies = remove_quotes_and_whitespace_from_list(
+        self._list_of_unprocessed_dependencies = _remove_quotes_and_whitespace_from_list(
             self._list_of_unprocessed_dependencies)
-        self._list_of_unprocessed_dependencies = filter_list_for_regex(
+        self._list_of_unprocessed_dependencies = _filter_list_for_regex(
             self._list_of_unprocessed_dependencies, self._ignore_this)
         self._list_of_dependencies = \
-            [string_dependency_to_dict(dependency)
+            [_string_dependency_to_dict(dependency)
              for dependency in self._list_of_unprocessed_dependencies]
 
     def check_file(self):
-        file_contents = get_file_contents(self._path)
+        file_contents = _get_file_contents(self._path)
         self._get_append_list(file_contents)
         self._get_install_requires_field_contents(file_contents)
         self._process_dependency_list()
@@ -178,7 +178,7 @@ class PythonSetuptoolsDependencyCheckerForDir():
         self._result = []
 
     def check_all_filename_in_subdirs(self):
-        """receives a path and setup filename to search for,
+        """Receives a path and setup filename to search for,
         a field dependency name and regex to ignore
         returns a dict of filename and the list of dependencies it uses,
         whether they are version locked,
